516f4282aa7a301a4b16317bdd0a3076c184857e
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-07-29 21:33:02 -0400

    fetch is not playing nice with unpacking binary data from the servers uint8clampedarray so not im going to buy a bottle of wine and throw my laptiosmpotpp out the window kjfnsnlkflknsfnkl

--- code/browser-canvas-client/conn.js
+++ code/browser-canvas-client/conn.js
@@ -1,30 +1,46 @@
 const BASE = 'http://localhost:3000'
 
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext('2d');
+
 function getBoard(id='0') {
-  fetch(BASE + `/getBoard?id=${id}`, {
-    method: 'GET' // *GET, POST, PUT, DELETE, etc.
-  })
-  .then(response => response.json())
-  .then(x => console.log(x))
-}
-
-function setTile(x, y, c='#FF0000', id='0') {
-  fetch(BASE + `/setTile?x=${x}&y=${y}&c=${c}&id=${id}`, {
-    method: 'Post',
-    mode: 'no-cors',
-  })
-  .then(response => response)
-  .then(x => { console.log(x.headers) })
-}
-
-function getTile(x, y, id='0') {
-  fetch(BASE + `/getTile?x=${x}&y=${y}&id=${id}`, {
+  fetch(BASE + `/board?id=${id}`, {
     method: 'GET',
-    mode: 'no-cors',
   })
-  .then(response => response)
-  .then(x => { console.log(x.headers.get("color")) })
+  .then(response => response.arrayBuffer()).then(function(buffer) {
+    console.log(buffer);
+    x = new Uint8ClampedArray(buffer)
+    // buffer = new Uint8ClampedArray(buffer, 28, 4)
+    // data = new ImageData(buffer, 1, 1)
+    // ctx.putImageData(data, 0, 0)
+  });
 }
+
+// var imageData = context.getImageData(x, y, w, h);
+// var array = imageData.data; // array is a Uint8ClampedArray
+// var buffer = imageData.data.buffer; // buffer is a ArrayBuffer
+// 
+// // incomingBuffer is a TypedArray
+// var imageData2 = context.createImageData(w, h);
+// imageData2.data.set(incomingBuffer);
+
+// function setTile(x, y, c='#FF0000', id='0') {
+//   fetch(BASE + `/tile?x=${x}&y=${y}&c=${c}&id=${id}`, {
+//     method: 'Post',
+//     mode: 'no-cors',
+//   })
+//   .then(response => response)
+//   .then(x => { console.log(x.headers) })
+// }
+
+// function getTile(x, y, id='0') {
+//   fetch(BASE + `/tile?x=${x}&y=${y}&id=${id}`, {
+//     method: 'GET',
+//     mode: 'no-cors',
+//   })
+//   .then(response => response)
+//   .then(x => { console.log(x.headers.get("color")) })
+// }
 
 // function fourOhFour() {
 //   fetch(BASE + '/nonsense', {
