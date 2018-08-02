2581c4abdf47051e26059a59f90a3fcc9660f8de
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-08-02 19:35:23 -0400

    updated tests to override config values and tightened up test to be specific to the board

--- code/node-server/test/game.test.js
+++ code/node-server/test/game.test.js
@@ -1,10 +1,11 @@
 const config = require('../config.js')
+config.ROWS = 10
+config.COLUMNS = 10
 const Game = require('../src/app/game.js')
 const Canvas = require('../src/app/canvas.js')
 const hex6CharToInt32 = require('../src/app/canvasHelpers/hex6CharToInt32.js')
 
 const game = new Game()
-const canvas = game.canvas
 
 test('creates a canvas', () => {
   expect(game.canvas instanceof Canvas).toBe(true)
@@ -16,18 +17,16 @@ test('properly updates the canvas with setTile and asserts that both views into 
   
   game.setTile({x: 0, y: 0, hexStr})
   
-  // TODO: these should really be in the canvas too
-  expect(canvas.int32View[0]).toBe(int32)
-  expect(canvas.int8View[0]).toBe(135)
-  expect(canvas.int8View[1]).toBe(254)
-  expect(canvas.int8View[2]).toBe(33)
-  expect(canvas.int8View[3]).toBe(255)
+  expect(game.getTile({x: 0, y: 0, hexStr})).toBe(int32)
+  expect(game.canvas.int8View[0]).toBe(135)
+  expect(game.canvas.int8View[1]).toBe(254)
+  expect(game.canvas.int8View[2]).toBe(33)
+  expect(game.canvas.int8View[3]).toBe(255)
   
   game.setTile({x: 1, y: 1, hexStr})
-  console.log(canvas.int32View[3].class)
-  expect(canvas.int32View[3]).toBe(int32)
-  expect(canvas.int8View[12]).toBe(135)
-  expect(canvas.int8View[13]).toBe(254)
-  expect(canvas.int8View[14]).toBe(33)
-  expect(canvas.int8View[15]).toBe(255)
+  expect(game.getTile({x: 0, y: 0, hexStr})).toBe(int32)
+  expect(game.canvas.int8View[44]).toBe(135)
+  expect(game.canvas.int8View[45]).toBe(254)
+  expect(game.canvas.int8View[46]).toBe(33)
+  expect(game.canvas.int8View[47]).toBe(255)
 })
