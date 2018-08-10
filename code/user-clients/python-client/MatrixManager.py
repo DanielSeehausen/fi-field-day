6d034142e08d867db090bcba2df12d5f99f3ac7b
Jonathan Mines <minesja@gmail.com>
2018-08-09 21:11:50 -0400

    Built out most methods. Need to finish set_tile. May need to refactor to use clear_queue structure

--- code/user-clients/python-client/MatrixManager.py
+++ code/user-clients/python-client/MatrixManager.py
@@ -1,19 +1,20 @@
 import json
-import urllib.request
+from urllib import request, parse
+
 
 class MatrixManager:
-    def __init__(id):
+    def __init__(self, id):
         self.id = id
         self.api_url = "https://dis-stevens-job/"
 
 
-    def get_tile(dict):
+    def get_tile(self, dict):
         # GET /get-tile?x=0&y=0
         # Should we use try: except: to raise errors instead of ifelse?
 
         if (type(dict) is dict):
             get_tile_url = self.api_url + "/get-tile?x=" + dict.x + "&y=" + dict.y
-            response = urllib.request.urlopen(get_tile_url)
+            response = request.urlopen(get_tile_url)
             data = response.read()
             encoding = response.info().get_content_charset('utf-8')
             payload = json.loads(data.decode(encoding))
@@ -22,14 +23,14 @@ class MatrixManager:
             return payload
 
         else:
-            raise ValueError("Expected a dict. Got not a dict. Not cool.")
+            raise ValueError("Expected a dict. Got no dict.")
 
 
-    def get_queue():
+    def get_queue(self):
         # GET /queue
 
         get_queue_url = self.api_url + "/queue"
-        response = urllib.request.urlopen(get_queue_url)
+        response = request.urlopen(get_queue_url)
         data = response.read()
         encoding = response.info().get_content_charset('utf-8')
         payload = json.loads(data.decode(encoding))
@@ -38,27 +39,59 @@ class MatrixManager:
         return payload
 
 
-     def clear_queue():
-        # DELETE /clear-queue
+    def clear_queue(self):
 
         clear_queue_url = self.api_url + "/clear-queue"
-        request = urllib2.Request(clear_queue_url)
-        request.get_method = lambda: 'DELETE'
-        response = urllib2.urlopen(request)
+        # response = request.urlopen(clear_queue_url)
+        # response.get_method = lambda: 'DELETE'
+        # data = response.read()
+        # encoding = response.info().get_content_charset('utf-8')
+        # payload = json.loads(data.decode(encoding))
+        # return payload
+
+        headers = {
+            'Content-Type': 'application/json;charset=UTF-8'
+        }
+
+        req = request.Request(
+            clear_queue_url,
+            headers=headers,
+            method='DELETE'
+        )
+
+        try:
+            response = urllib.request.urlopen(req)
+            return("Success: " + response.read().decode('utf8'))
+        except urllib.error.URLError as error:
+            return("Error: " + error.read().decode('utf8'))
+
+
+
+    def get_rect(self, start, end):
+        # GET /get-rect?x1=0&y1=0&x2=1&y2=1
+
+        get_rect_queue_url = self.api_url + "/get-rect?x1=" + start.x + "&y1=" + start.y + "&x2=" + end.x + "&y2=" + end.y
+        response = request.urlopen(get_rect_queue_url)
         data = response.read()
         encoding = response.info().get_content_charset('utf-8')
         payload = json.loads(data.decode(encoding))
-        # Response {message: “Success”, number_deleted: <INT>}
+        # Response: [{x,y,c}, {x,y,c}]
 
         return payload
 
-
-    def get_rect():
-        # GET /get-rect?x1=0&y1=0&x2=1&y2=1
-        # Response: [{x,y,c}]
-
-
-    def set_tile:
+    def set_tile(self, dict):
         # POST /set-tile
         #     Body: {x,y,c}
-        #     Response: {coordinate: {x,y,c}, message: “QUEUED”, queue-position: <INT>}
+        #     Response: {coordinate: {x,y,c}, message: “QUEUED”, queue-position: int}
+
+        set_tile_url = self.api_url + "/set-tile"
+        data = parse.urlencode(dict).encode()
+        req =  request.Request(set_tile_url, data=data) # this will make the method "POST"
+        resp = request.urlopen(req)
+
+        
+
+
+
+
+        #
--- code/user-clients/python-client/README.md
+++ code/user-clients/python-client/README.md
@@ -1,1 +1,8 @@
-
+# Response {message: “Success”, number_deleted: <INT>}
+# def get_rect():
+#     # GET /get-rect?x1=0&y1=0&x2=1&y2=1
+#     # Response: [{x,y,c}]
+#
+#
+# def set_tile:
+  
