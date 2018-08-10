43762798cc829e114c4776bd971b6e75efe25782
sbal13 <sbal13@gmail.com>
2018-08-10 10:26:16 +0100

    quick change

--- code/user-clients/nexus/socket-server.js
+++ code/user-clients/nexus/socket-server.js
@@ -3,50 +3,50 @@ const socketURI = "ws://mayisgr8.win:8080"
 
 //WSS Manager
 const WSSManager = (function (){
-	let board = []
+  let board = []
 
-	return class {
+  return class {
 
-		constructor(){
+    constructor(){
 
-			// INITIALIZER DISPATCH
-			this.dispatch = {
-				"setBoard": this.writeBoard,
-				"setTile": this.writeTile,
-			}
+      // INITIALIZER DISPATCH
+      this.dispatch = {
+        "setBoard": this.writeBoard,
+        "setTile": this.writeTile,
+      }
 
-			// BIND CONTEXT 
-			this.writeTile = this.writeTile.bind(this)
-			this.writeBoard = this.writeBoard.bind(this)
-			this.handleMsg = this.handleMsg.bind(this)
-			this.requestBoard = this.requestBoard.bind(this)
+      // BIND CONTEXT 
+      this.writeTile = this.writeTile.bind(this)
+      this.writeBoard = this.writeBoard.bind(this)
+      this.handleMsg = this.handleMsg.bind(this)
+      this.requestBoard = this.requestBoard.bind(this)
 
-			// INITIALIZE WS CONNECTION & ATTACH LISTENERS
-			this.wsConn = new WebSocket(socketURI)
-			this.wsConn.addEventListener('open', this.requestBoard)
-			this.wsConn.addEventListener('message', this.handleMsg)
-			this.wsConn.addEventListener('close', () => console.log('Socket closed'))
-		}
+      // INITIALIZE WS CONNECTION & ATTACH LISTENERS
+      this.wsConn = new WebSocket(socketURI)
+      this.wsConn.addEventListener('open', this.requestBoard)
+      this.wsConn.addEventListener('message', this.handleMsg)
+      this.wsConn.addEventListener('close', () => console.log('Socket closed'))
+    }
 
-		writeTile(obj){
-		}
+    writeTile(obj){
+    }
 
-		writeBoard(arr){
-		}
+    writeBoard(arr){
+    }
 
 
-		handleMsg(msg){
-			const {action, payload} = JSON.parse(msg.data)
-			this.dispatch[action](payload)
-		}
+    handleMsg(msg){
+      const {action, payload} = JSON.parse(msg.data)
+      this.dispatch[action](payload)
+    }
 
-		requestBoard(){
-			this.wsConn.send(JSON.stringify({
-				action: "getBoard", 
-				payload: null
-			}))
-		}
-	}
+    requestBoard(){
+      this.wsConn.send(JSON.stringify({
+        action: "getBoard", 
+        payload: null
+      }))
+    }
+  }
 })()
 
 
