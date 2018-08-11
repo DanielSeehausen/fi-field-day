baa5910190f1aa456bf17d62c73ee742fd492275
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-08-11 15:41:02 -0400

    x

--- code/browser-canvas-client/SocketConn.js
+++ code/browser-canvas-client/SocketConn.js
@@ -7,10 +7,7 @@ class SocketConn {
   connectWS() {
     const socket = new WebSocket(this.endpoint)
     console.log('new ws')
-    socket.addEventListener('open', event => {
-      socket.send('Hello Server!')
-    })
-
+    
     socket.addEventListener('message', event => {
       console.log('Message from server ', event.data)
     })
