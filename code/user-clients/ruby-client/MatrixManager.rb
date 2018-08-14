0ac5e565489d9eb1d33f96a3ab87219583f55fc4
Jonathan Mines <minesja@gmail.com>
2018-08-13 22:14:23 -0400

    Almost finished Ruby client. Just need to finish delete

--- code/user-clients/ruby-client/MatrixManager.rb
+++ code/user-clients/ruby-client/MatrixManager.rb
@@ -1,123 +1,53 @@
-require 'redis'
 require 'rest-client'
 require 'json'
 
 class MatrixManager
+	attr_reader :api_url
+
 	def initialize(id)
-		@redis = Redis.new(port: 6379)
-		@server_url = "http://mayisgr8.win"
-		@id = id
-		@last_write = false
-	end
-
-	def initialize_blank(size=1000)
-		 self.set_rect({x:0, y:0}, {x: size-1,y: size-1}, "000")
-	end
-
-	def read(argument)
-		if argument.class == Array
-			coordinates = self.many_coordinates(argument)
-			colors = @redis.mget(*coordinates)
-
-			argument.map.with_index do |point, i|
-				{x: point[:x], y: point[:y], color: colors[i]}
-			end
-		elsif argument.class == Hash
-			coordinate = self.to_coordinate(argument)
-			color = @redis.get(coordinate)
-
-			{x: argument[:x], y: argument[:y], color: color}
-		end
+  	@id = id
+    @api_url = "http://localhost:3000"
 	end
 
 
-	def read_rect(start_point, end_point)
-		self.read(self.create_rect(start_point, end_point))
+
+	def set_tile(hash)
+		set_tile_url = "#{self.api_url}/set-tile"
+		response = RestClient.post(set_tile_url, hash)
+		JSON.parse(response)
 	end
 
-	def set_rect(start_point, end_point, color, local=false)
-		data = self.create_rect(start_point, end_point)
 
-		if local
-			self.local_set(data)
-		else
-			self.set(data, color)
-		end
+	def clear_queue
+		delete_queue_url = "#{self.api_url}/clear_queue"
+		binding.pry
+		response = RestClient.delete(delete_queue_url)
+		JSON.parse(response)
 	end
 
-	def set(argument, color)
 
-		if (@last_write && !(Time.now - @last_write >= 0.3))
-			return
-		end
-
-		if argument.class == Array
-			argument.each do |point|
-				self.send(point[:x], point[:y], point[:color] || color)
-				@last_write = Time.now
-				sleep(0.31)
-			end
-		elsif argument.class == Hash
-			self.send(argument[:x], argument[:y], argument[:color] || color)
-			@last_write = Time.now
-		end
-
+	def get_queue
+		get_queue_url = "#{self.api_url}/get-queue"
+		response = RestClient.get(get_queue_url)
+		JSON.parse(response)
 	end
 
-	def local_set(argument)
-		if argument.class == Array
-			coordinates = self.many_coordinates(argument, true)
-			coordinates.each do |coord|
-				@redis.set(coord[:coordinate], coord[:color])
-			end
-		elsif argument.class == Hash
-			coordinate = self.to_coordinate(argument)
-			@redis.set(coordinate, argument[:color])
-		end
+
+	def get_rect(start_hash, end_hash)
+		# End point not live yet
+		get_rect_queue_url = "#{self.api_url}/get-rect?x1=#{start_hash[:x]}&y1=#{start_hash[:y]}&x2=#{end_hash[:x]}&y2=#{end_hash[:y]}"
+		response = RestClient.get(get_rect_queue_url)
+		JSON.parse(response)
 	end
 
-	def send(x,y,color)
-		puts ("Sending #{color} to coordinates (#{x},#{y})!")
-		begin
-			RestClient.post(@server_url + "/setTile?x=#{x}&y=#{y}&c=#{color}&id=#{@id}", {})
-		rescue
-			send(x,y,color)
-		end
+
+	def get_tile(hash)
+		get_tile_url = "#{self.api_url}/get-tile?x=#{hash[:x]}&y=#{hash[:y]}"
+		response = RestClient.get(get_tile_url)
+		JSON.parse(response)
 	end
 
-	def create_rect(start_point, end_point, color=nil)
-		points = []
-		if end_point[:x] > start_point[:x] && end_point[:y] > start_point[:y]
-				(start_point[:x]..end_point[:x]).each do |x|
-					(start_point[:y]..end_point[:y]).each do |y|
-						if color
-							points << {x: x, y: y, color: color}
-						else
-							points << {x: x, y: y}
-						end
-					end
-				end
-		end
 
-		points
-	end
 
-	def many_coordinates(array_hash, set=false)
-
-		array_hash.map do |hash|
-			coordinate = self.to_coordinate(hash)
-
-			if set
-				{coordinate: coordinate, color: hash[:color]}
-			else
-				coordinate
-			end
-		end
-	end
-
-	def to_coordinate(hash)
-		# {x:5,y:4} => "5-4"
-		"#{hash[:x]}-#{hash[:y]}"
-	end
 
 end
