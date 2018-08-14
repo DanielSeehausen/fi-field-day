f3d2a21086f685d7f7481cf597c506ee1220e44d
gonzodamus <github email address>
2018-08-14 10:43:06 -0400

    Zoom Tiers #41

--- code/browser-canvas-client/CanvasManager.js
+++ code/browser-canvas-client/CanvasManager.js
@@ -1,23 +1,28 @@
 class CanvasManager{
 
   constructor(){
-    this.zoomer = document.getElementById("zoomer")
+    this.zoomer = document.getElementById(`zoomer`)
+    this.zoomStatus = document.getElementById(`zoomStatus`)
   }
 
   cycleZoom(){
     switch (this.zoomer.style.transform) {
-      case "scale(7, 7)":
-        this.zoomer.style.transform = `scale(20, 20)`
+      case `scale(7, 7)`:
+        this.zoomer.style.transform = `scale(14, 14)`
+        this.zoomStatus.innerText = `Current Zoom: x2`
         break;
-      case "scale(20, 20)":
-        this.zoomer.style.transform = `scale(40, 40)`
+      case `scale(14, 14)`:
+        this.zoomer.style.transform = `scale(28, 28)`
+        this.zoomStatus.innerText = `Current Zoom: x4`
         break;
-      case "scale(40, 40)":
+      case `scale(28, 28)`:
         this.zoomer.style.transform = `scale(7, 7)`
+        this.zoomStatus.innerText = `Current Zoom: x1`
         break;
       default:
       this.zoomer.style.transform = `scale(7, 7)`
+      this.zoomStatus.innerText = `Current Zoom: x1`
     }
   }
-  
+
 }
