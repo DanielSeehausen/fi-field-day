cfc34ec99790d3bbb894294d1949b85db61d31fb
sbal13 <sbal13@gmail.com>
2018-08-23 21:21:47 +0100

    Nexus (#50)
    * express routing and socket manager class
    
    * quick change
    
    * nexus changes
    
    * updated WS in browser-client

--- code/user-clients/nexus/WSClient.js
+++ code/user-clients/nexus/WSClient.js
@@ -1,21 +1,50 @@
-const WSClient = require('ws').client
-const config = require('./config.js')
-
+const WebSocket = require('ws')
+const config = require("./config.js")
+const socketURI = `ws://localhost:${config.SERVERWSPORT}`
 
 class WSClient {
-  
-  constructor(setTile) {
-    this.board = board
-    this.wsConn = new WebSocket(config.SERVERWSPORT)
-    this.wsConn.addEventListener('message', setTile)
-    this.wsConn.addEventListener('close', () => console.log('Socket closed'))
-  }
-  
-  setTile(data) {
-    //TODO set tile here. 
-  }
+		constructor(board, writeTile){
 
-}
+			// INITIALIZER DISPATCH
+			this.dispatch = {
+				"writeTile": writeTile,
+			}
 
+			this.board = board
+
+			// BIND CONTEXT 
+			this.handleMsg = this.handleMsg.bind(this)
+
+			// INITIALIZE WS CONNECTION & ATTACH LISTENERS
+			this.wsConn = new WebSocket(socketURI)
+			this.wsConn.on('open', () => console.log("CONNECTED"))
+			this.wsConn.on('message', this.handleMsg)
+			this.wsConn.on('close', () => console.log('Socket closed'))
+		}
+
+		convertBoard(){
+			let array = []
+
+			for (var key in this.board) {
+				let coordinates = key.split("-")
+				let x = parseInt(coordinates[0], 10)
+				let y = parseInt(coordinates[1], 10)
+				array.push({x,y, color: this.board[key]})
+			}
+			return array
+		}
+
+		handleMsg(msg){
+			let data = JSON.parse(msg)
+			console.log(data)
+			const {action, payload} = data
+
+			if (action && payload)
+				this.dispatch[action](payload)
+
+		}
+
+
+	}
 
 module.exports = WSClient
