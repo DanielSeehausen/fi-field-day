38f225c31a28e0e117c4f44f81efef5889be0348
sbal13 <sbal13@gmail.com>
2018-09-02 18:11:57 +0100

    Enabled CORS in Express

--- code/nexus/index.js
+++ code/nexus/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const url = require('url');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
@@ -9,6 +10,7 @@ const config = require("./config.js")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 // CONFIG
 const size = config.BOARDDIMENSION
@@ -17,68 +19,68 @@ const teamID = config.GROUPID
 const HTTPEndpoint = config.APIENDPOINT
  
 // GLOBAL VARS
-const queue = []
+let queue = []
 
 // game
 const game = new Game()
 
 // HELPERS
 function startInterval() {
-  setInterval(() => {
-    // console.log(queue)
-    if (queue.length > 0) {
-      let nextPoint = queue.shift()
-      console.log("SENDING ", nextPoint)
-      game.setTile(nextPoint.x, nextPoint.y, nextPoint.c)
-    }
-  }, config.INTERVAL)
+	setInterval(() => {
+		// console.log(queue)
+		if (queue.length > 0) {
+			let nextPoint = queue.shift()
+			console.log("SENDING ", nextPoint)
+			game.setTile(nextPoint.x, nextPoint.y, nextPoint.c)
+		}
+	}, config.INTERVAL)
 }
 startInterval()
 
 function checkValidPoint(x,y) {
-  const integers = Number.isInteger(x) && Number.isInteger(y)
-  const inRange = x >= 0 && y >= 0 && x < size && y < size 
-  return integers && inRange
+	const integers = Number.isInteger(x) && Number.isInteger(y)
+	const inRange = x >= 0 && y >= 0 && x < size && y < size 
+	return integers && inRange
 }
 
 
 // ROUTES
 app.get('/get-tile', (req, res) => {
-  const x = parseInt(req.query.x, 10)
-  const y = parseInt(req.query.y, 10)
-  if (checkValidPoint(x,y)) {
-    let color = game.board[`${x}-${y}`]
-    res.send({x,y,color})
-  } else {
-    res.send({error: "Invalid query parameters."})
-  }
+	const x = parseInt(req.query.x, 10)
+	const y = parseInt(req.query.y, 10)
+	if (checkValidPoint(x,y)) {
+		let color = game.board[`${x}-${y}`]
+		res.send({x,y,color})
+	} else {
+		res.send({error: "Invalid query parameters."})
+	}
 })
 
 app.get("/board", (req, res) => {
-  res.send(game.convertBoard())
+	res.send(game.convertBoard())
 })
 
 app.post('/set-tile', (req, res) => {
-  const x = req.body.x
-  const y = req.body.y
-  const c = req.body.c
-  if (checkValidPoint(x,y)) {
-    const coordinate = {x, y, c}
-    queue.push(coordinate)
-    res.send({success: "Successfully queued!", coordinate, position: queue.length})
-  } else {
-    res.send({error: "Invalid point."})
-  }
+	const x = req.body.x
+	const y = req.body.y
+	const c = req.body.c
+	if (checkValidPoint(x,y)) {
+		const coordinate = {x, y, c}
+		queue.push(coordinate)
+		res.send({success: "Successfully queued!", coordinate, position: queue.length})
+	} else {
+		res.send({error: "Invalid point."})
+	}
 })
 
 app.get('/get-queue', (req, res) => {
-  res.send(queue)
+	res.send(queue)
 })
 
 app.delete('/clear-queue', (req, res) => {
-  let numItemsRemoved = queue.length
-  queue = []
-  res.send({message: `Queue successfully cleared. ${numItemsRemoved} items removed.`})
+	let numItemsRemoved = queue.length
+	queue = []
+	res.send({message: `Queue successfully cleared. ${numItemsRemoved} items removed.`})
 })
 
 
