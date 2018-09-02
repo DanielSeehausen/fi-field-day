a7064971a836e3be1059bf7c2c83e12468e3c911
Gonzodamus <Dick@TheDickWard.com>
2018-09-02 10:12:29 -0400

    zoom is now dynamic based on window height

--- code/browser-canvas-client/config.js
+++ code/browser-canvas-client/config.js
@@ -1,8 +1,8 @@
 const config = {
   DEFAULTLENGTH: 500,
   DEFAULTARRAY: null,
-  HTTPENDPOINT: 'http://theAPI.link',
-  WSENDPOINT: 'ws://theAPI.link/socket',
+  HTTPENDPOINT: 'http://localhost:3000',
+  WSENDPOINT: 'ws://localhost:8080',
   ID: 0 //preassigned group ids
 }
 
