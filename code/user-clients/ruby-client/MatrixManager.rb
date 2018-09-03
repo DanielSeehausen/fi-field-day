283d16bee994fc9854b7bf9fffa28c08a6704d77
sbal13 <sbal13@gmail.com>
2018-09-03 02:21:07 +0100

    Nexus (#70)
    * express routing and socket manager class
    
    * quick change
    
    * nexus changes
    
    * updated WS in browser-client
    
    * yoyo
    
    * moved fetches from game.js to HTTPConn in Nexus
    
    * changed hardcoded ID to use config file
    
    * Enabled CORS in Express
    
    * nexus color validations
    
    * updated nexus to receive IDs in request and updated node and ruby clients to send them

--- code/user-clients/ruby-client/MatrixManager.rb
+++ code/user-clients/ruby-client/MatrixManager.rb
@@ -2,51 +2,52 @@ require 'rest-client'
 require 'json'
 
 class MatrixManager
-	attr_reader :api_url
+  attr_reader :api_url
 
-	def initialize(id)
-  	@id = id
-    @api_url = "http://localhost:3000"
-	end
+  def initialize(id)
+    @id = id
+    @api_url = "http://localhost:3001"
+  end
 
-	def set_tile(hash)
-		# Tested, working
-		set_tile_url = "#{self.api_url}/set-tile"
-		response = RestClient.post(set_tile_url, hash)
-		JSON.parse(response)
-	end
-
-
-	def clear_queue
-		# Tested, working
-		delete_queue_url = "#{self.api_url}/clear-queue"
-		response = RestClient.delete(delete_queue_url)
-		JSON.parse(response)
-	end
+  def set_tile(hash)
+    # Tested, working
+    hash[:id] = @id
+    set_tile_url = "#{self.api_url}/set-tile"
+    response = RestClient.post(set_tile_url, hash)
+    JSON.parse(response)
+  end
 
 
-	def get_queue
-		# Tested, working
-		get_queue_url = "#{self.api_url}/get-queue"
-		response = RestClient.get(get_queue_url)
-		JSON.parse(response)
-	end
+  def clear_queue
+    # Tested, working
+    delete_queue_url = "#{self.api_url}/clear-queue"
+    response = RestClient.delete(delete_queue_url)
+    JSON.parse(response)
+  end
 
 
-	def get_rect(start_hash, end_hash)
-		# End point not live yet
-		get_rect_queue_url = "#{self.api_url}/get-rect?x1=#{start_hash[:x]}&y1=#{start_hash[:y]}&x2=#{end_hash[:x]}&y2=#{end_hash[:y]}"
-		response = RestClient.get(get_rect_queue_url)
-		JSON.parse(response)
-	end
+  def get_queue
+    # Tested, working
+    get_queue_url = "#{self.api_url}/get-queue"
+    response = RestClient.get(get_queue_url)
+    JSON.parse(response)
+  end
 
 
-	def get_tile(hash)
-		# Tested, working
-		get_tile_url = "#{self.api_url}/get-tile?x=#{hash[:x]}&y=#{hash[:y]}"
-		response = RestClient.get(get_tile_url)
-		JSON.parse(response)
-	end
+  def get_rect(start_hash, end_hash)
+    # End point not live yet
+    get_rect_queue_url = "#{self.api_url}/get-rect?x1=#{start_hash[:x]}&y1=#{start_hash[:y]}&x2=#{end_hash[:x]}&y2=#{end_hash[:y]}"
+    response = RestClient.get(get_rect_queue_url)
+    JSON.parse(response)
+  end
+
+
+  def get_tile(hash)
+    # Tested, working
+    get_tile_url = "#{self.api_url}/get-tile?x=#{hash[:x]}&y=#{hash[:y]}"
+    response = RestClient.get(get_tile_url)
+    JSON.parse(response)
+  end
 
 
 
