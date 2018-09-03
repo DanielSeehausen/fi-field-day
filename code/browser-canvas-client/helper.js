40a7f13da6dfdf98ccad416763b8c28d30a4c8a1
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-09-03 18:20:09 -0400

    merge conflicts fixed, mixed dick's update as well as this one

--- code/browser-canvas-client/helper.js
+++ code/browser-canvas-client/helper.js
@@ -8,12 +8,18 @@ function showModal() {
   modal.style.zIndex = 10
   modal.focus()
   modalOpen = true
+  
+  button.style.opacity = 0
+  button.style.zIndex = -1
 }
 
 function hideModal() {
   modal.style.opacity = 0
   modal.style.zIndex = -1
   modalOpen = false
+  
+  button.style.opacity = .5
+  button.style.zIndex = 10
 }
 
 function onButtonClick() {
@@ -22,3 +28,18 @@ function onButtonClick() {
 
 button.onclick = onButtonClick
 modal.onblur = hideModal
+
+// /********************* Instruction Pane Handler ***************************************/
+// 
+// const questionMark = document.getElementById('questionMark')
+// const instructions = document.getElementsByClassName('instructionsHidden')[0]
+// questionMark.addEventListener('click', (e) => {
+//   instructions.className = "instructions fixed bottom-0 left-0 right-0 mr2 right"
+//   questionMark.className = "questionMarkHidden right mr2"
+// })
+// 
+// const close = document.getElementById('close')
+// close.addEventListener('click', (e) =>{
+//   instructions.className = "instructionsHidden fixed bottom-0 left-0 right-0 mr2 right"
+//   questionMark.className = "questionMark right mr2"
+// }){{}}
