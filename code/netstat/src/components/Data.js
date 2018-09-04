386f8dca4e7c5dbf6a35dcdfbfc541b1fd8ca69d
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2018-09-04 18:12:36 -0400

    Netstat react app (#74)
    * create react app for netstat, basic styling and set up a bar graph with d3 functionality
    
    * will my dank react app upload?

---
+++ code/netstat/src/components/Data.js
@@ -0,0 +1,15 @@
+export default [
+  { title: 'Group 1', hits: 600 },
+  { title: 'Group 2', hits: 300 },
+  { title: 'Group 3', hits: 200 },
+  { title: 'Group 4', hits: 400 },
+  { title: 'Group 5', hits: 50 },
+  { title: 'Group 6', hits: 75 },
+  { title: 'Group 7', hits: 83 },
+  { title: 'Group 8', hits: 91 },
+  { title: 'Group 9', hits: 100 },
+  { title: 'Group 10', hits: 540 },
+  { title: 'Group 11', hits: 320 },
+  { title: 'Group 12', hits: 10 },
+  { title: 'Group 13', hits: 200 },
+]
