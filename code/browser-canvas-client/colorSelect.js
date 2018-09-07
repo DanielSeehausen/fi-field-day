8adfcc8e3bc9edfe00f461e9db51728adec4ceed
Gonzodamus <Dick@TheDickWard.com>
2018-09-07 19:49:12 -0400

    need to finish colorForm event listener

--- code/browser-canvas-client/colorSelect.js
+++ code/browser-canvas-client/colorSelect.js
@@ -2,7 +2,16 @@ const colorPicker = document.getElementById('colorPicker')
 const pick = document.getElementById('pick')
 const paint = document.getElementById('paint')
 let hexColor = "#000000"
+let colorForm = document.getElementById('colorForm')
+let hex = document.getElementById('hex')
 
+colorForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(hex.value)
+  colorPicker.style.backgroundColor = hex.value
+  // colorPicker.style.outlineColor = invertColor(hex.value)
+  // console.log(hex.value)
+})
 
 function componentToHex(c) {
     var hex = c.toString(16);
@@ -60,6 +69,32 @@ function handleClick(e){
     applyColor(coords)
   }
 }
+
+function invertColor(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
+
 
 
 // function handleClicks(e) {
