5fb6a3fece332635f5f24e2391222ed1c751e417
Andrew Cohn <andrewcohn0@gmail.com>
2018-09-07 19:18:26 -0400

    THICCCCCCCCC

--- code/node-server/src/middleware/logger.js
+++ code/node-server/src/middleware/logger.js
@@ -4,10 +4,10 @@ const errStream = fs.createWriteStream('logs/http-req-error.log', { flags: 'a' }
 
 const logger = (err, req, res, next) => {
   if (err) {
-    errStream.write(`\nErr: ${err.message} Req Conn: ${req.connection.remoteAddress} Req Url: ${req.url} Time: ${Date.now()}`)
+    errStream.write(`\nErr: ${err.message} Req Conn: ${req.connection.remoteAddress} ** Time: ${Date.now()} | Endpoint: ${req.url} | GroupID: ${req.query.id} | X: ${req.query.x} Y: ${req.query.y} Color: ${req.query.c} **`)
     next(err)
   }
-  logStream.write(`\nReq Conn: ${req.connection.remoteAddress} Req Url: ${req.url} Time: ${Date.now()}`)
+  logStream.write(`\nReq Conn: ${req.connection.remoteAddress} ** Time: ${Date.now()} | Endpoint: ${req.url} | GroupID: ${req.query.id} | X: ${req.query.x} Y: ${req.query.y} Color: ${req.query.c} **`)
   next()
 }
 
