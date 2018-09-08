1dfb61ef99d8f069761620f6fd4fe317d12def37
Andrew Cohn <andrewcohn0@gmail.com>
2018-09-07 20:58:42 -0400

    cheque 4 nums

--- code/node-server/src/middleware/reqValidators/validID.js
+++ code/node-server/src/middleware/reqValidators/validID.js
@@ -2,7 +2,7 @@ const config = require('../../../config.js')
 
 function validID(req) {
   const id = parseInt(req.query.id)
-  return id < config.IDLIMIT['low'] || id > config.IDLIMIT['high'] ? false : true
+  return (typeof id !== 'number' || (id < config.IDLIMIT['low'] || id > config.IDLIMIT['high'])) ? false : true
 }
 
 module.exports = validID
