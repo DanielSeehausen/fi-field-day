4cadebd24fc5d5d96f71ba33be84c50d8dcbd55b
Andrew Cohn <andrewcohn0@gmail.com>
2018-09-07 21:28:58 -0400

    BAD BOIS PUSH 2 MASTR

--- code/node-server/src/middleware/logger.js
+++ code/node-server/src/middleware/logger.js
@@ -1,12 +1,9 @@
 const fs = require('fs')
+console.log('PWD', process.cwd())
 const logStream = fs.createWriteStream('logs/http-req.log', { flags: 'a' })
 const errStream = fs.createWriteStream('logs/http-req-error.log', { flags: 'a' })
 
-const logger = (err, req, res, next) => {
-  if (err) {
-    errStream.write(`\n${err.message}|${req.connection.remoteAddress}|${Date.now()}|${req.url}|${req.query.id}|${req.query.x}|${req.query.y}|${req.query.c}`)
-    next(err)
-  }
+const logger = (req, res, next) => {
   logStream.write(`\n${req.connection.remoteAddress}|${Date.now()}|${req.url}|${req.query.id}|${req.query.x}|${req.query.y}|${req.query.c}`)
   next()
 }
